b63d133a3a0bb7fcf36cf16da148943a15bc6406
TheBude <husaenbobmuratov192@gmail.com>
2024-03-30 21:19:45 +0500

    Kalkulyotor

--- MohirDevTopshirq2/Program.cs
+++ MohirDevTopshirq2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.WriteLine("Assalomu allaykum menga uzingizni tanishtiring");
+
+Console.Write("Ismingiz: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Tanishganimdan Xursandman hurmatli {name} !\n Men +, -, *, / uchun muljalangan kalkulyotor dasturman.");
+
+Console.Write("1-sonni kiriting: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("bu son ustida qaysi amalni bajarmoqchisiz +, -, *, / kiritinhg: ");
+char amal = Convert.ToChar(Console.ReadLine());
+
+Console.Write("2-sonni kiriting: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int add = num + num1;
+int subtraction = num - num1;
+int multiply = num * num1;
+int divide = num / num1;
+
+Console.WriteLine($"Qo'shganda: {add}\nAyirganda: {subtraction}\nKo'paytirganda: {multiply}\nBo'lganda: {divide}");
