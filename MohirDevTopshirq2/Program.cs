dc225bd78be907d13dc7b25bfe074e6788ccc8b3
TheBude <husaenbobmuratov192@gmail.com>
2024-03-30 21:22:04 +0500

    Raqamlar qatorining yig‘indisini hisoblash:

--- MohirDevTopshirq2/Program.cs
+++ MohirDevTopshirq2/Program.cs
@@ -1,22 +1,10 @@
-﻿Console.WriteLine("Assalomu allaykum menga uzingizni tanishtiring");
+﻿Console.Write("Sonni kiriting: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ismingiz: ");
-string name = Console.ReadLine();
+int sum = 0;
+for (int i = 1; i <= N; i++)
+{
+    sum += i;
+}
 
-Console.WriteLine($"Tanishganimdan Xursandman hurmatli {name} !\n Men +, -, *, / uchun muljalangan kalkulyotor dasturman.");
-
-Console.Write("1-sonni kiriting: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("bu son ustida qaysi amalni bajarmoqchisiz +, -, *, / kiritinhg: ");
-char amal = Convert.ToChar(Console.ReadLine());
-
-Console.Write("2-sonni kiriting: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-int add = num + num1;
-int subtraction = num - num1;
-int multiply = num * num1;
-int divide = num / num1;
-
-Console.WriteLine($"Qo'shganda: {add}\nAyirganda: {subtraction}\nKo'paytirganda: {multiply}\nBo'lganda: {divide}");
+Console.WriteLine("Natija: " + sum);
